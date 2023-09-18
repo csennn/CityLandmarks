1294bd08c1414dfd08c9b6ae1042c3ae983c22ed
csennn <b2200356058@cs.hacettepe.edu.tr>
2023-09-18 03:23:05 +0300

    created constructor

---
+++ app/src/main/java/com/ren/citylandmarks/Landmark.java
@@ -0,0 +1,16 @@
+package com.ren.citylandmarks;
+
+public class Landmark {
+    String name;
+    String city;
+    int image;
+
+    //constructor (the first method called when creating an object from the class)
+
+    public Landmark(String name, String city, int image){
+        this.name = name;
+        this.city = city;
+        this.image = image;
+    }
+
+}
--- app/src/main/java/com/ren/citylandmarks/MainActivity.java
+++ app/src/main/java/com/ren/citylandmarks/MainActivity.java
@@ -10,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Landmark anitkabir = new Landmark("Anıtkabir", "Ankara", R.drawable.anitkabir_1);
+        Landmark coloseum = new Landmark("Colosseum", "Rome", R.drawable.colosseum_1);
+        Landmark eiffeltower = new Landmark("Eiffel Tower", "Paris", R.drawable.eiffeltower_1);
+        Landmark pisatower = new Landmark("Pisa Tower", "Pisa", R.drawable.pisatower_1);
+
+
     }
 }
